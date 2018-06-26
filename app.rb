67fb09d6021d4941fed54f20ac16490757ac1734
GraceAHerr <mazyg321@gmail.com>
2018-06-26 15:43:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,5 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name'do
     "My name is Grace"
+  get '/hometown' do
+    "My hometown is Forest Hill"
+  get '/favorite-song' do
+    "My favorite song is DNA"
   end
-  
+end
